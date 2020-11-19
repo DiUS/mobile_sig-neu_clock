09ccf703de7acd5fb93262c420355b89b4aaac42
Trevor Plant <tplant@dius.com.au>
2020-11-19 14:43:37 +1100

    Block out layout for widgets

--- lib/presentation/screens/clock_screen.dart
+++ lib/presentation/screens/clock_screen.dart
@@ -4,31 +4,55 @@ class ClockScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12),
-          child: AppBar(
-    title: Text(
-      'Alarm',
-      style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 24,
-            color: Theme.of(context).primaryColor),
-    ),
-    backgroundColor: Theme.of(context).backgroundColor,
-    elevation: 0,
-actions: [
-  IconButton(icon: Icon(Icons.settings, color: Theme.of(context).primaryColor), onPressed: () => {}, iconSize: 32,)
-],
+      appBar: AppBar(
+        title: Text(
+          'Alarm',
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
+              color: Theme.of(context).primaryColor),
+        ),
+        backgroundColor: Theme.of(context).backgroundColor,
+        elevation: 0,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.settings, color: Theme.of(context).primaryColor),
+            onPressed: () => {},
+            iconSize: 32,
+          )
+        ],
+      ),
+      body: Container(
+        color: Theme.of(context).backgroundColor,
+        child: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                height: 300,
+                color: Colors.blue[400],
+                child: Center(
+                  child: Text('ClockFace'),
+                ),
+              ),
+              Container(
+                height: 200,
+                child: Center(
+                  child: Text('Time List'),
+                ),
+                color: Colors.amber[400],
+              ),
+              Expanded(child: Container()),
+              Container(
+                height: 60,
+                child: Center(
+                  child: Text('Bottom Button Row'),
+                ),
+                color: Colors.pink[400],
+              )
+            ],
           ),
         ),
-        body: Container(
-    color: Theme.of(context).backgroundColor,
-    child: SafeArea(
-          child: Center(
-        child: Text('Neu Clock'),
       ),
-    ),
-        ),
-      );
+    );
   }
 }
