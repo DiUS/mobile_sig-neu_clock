7b67053e508c93166bbf193dfff1a263b2210f03
Trevor Plant <tplant@dius.com.au>
2020-11-22 16:16:12 +1100

    Style bottom nav bar

--- lib/presentation/widgets/bottom_nav_bar.dart
+++ lib/presentation/widgets/bottom_nav_bar.dart
@@ -1,41 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 class BottomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        _BottomButton(
-            icon: Icon(
-              Icons.language,
-              size: 32,
-              color: Theme.of(context).accentColor,
-            ),
-            cb: () => print('World Clocks')),
-        _BottomButton(
-            icon: Icon(
-              Icons.alarm,
-              size: 32,
-              color: Theme.of(context).accentColor,
-            ),
-            selected: true,
-            cb: () => print('Alarms')),
-        _BottomButton(
-            icon: Icon(
-              Icons.timer,
-              size: 32,
-              color: Theme.of(context).accentColor,
-            ),
-            cb: () => print('World Clocks')),
-        _BottomButton(
-            icon: Icon(
-              Icons.timer,
-              size: 32,
-              color: Theme.of(context).accentColor,
-            ),
-            cb: () => print('World Clocks')),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _BottomButton(
+              icon: Icon(
+                Icons.language,
+                size: 32,
+                color: NeumorphicTheme.currentTheme(context).accentColor,
+              ),
+              cb: () => print('World Clocks')),
+          _BottomButton(
+              icon: Icon(
+                Icons.alarm,
+                size: 32,
+                color: NeumorphicTheme.currentTheme(context).accentColor,
+              ),
+              selected: true,
+              cb: () => print('Alarms')),
+          _BottomButton(
+              icon: Icon(
+                Icons.timer,
+                size: 32,
+                color: NeumorphicTheme.currentTheme(context).accentColor,
+              ),
+              cb: () => print('World Clocks')),
+          _BottomButton(
+              icon: Icon(
+                Icons.timer,
+                size: 32,
+                color: NeumorphicTheme.currentTheme(context).accentColor,
+              ),
+              cb: () => print('World Clocks')),
+        ],
+      ),
     );
   }
 }
@@ -53,16 +57,24 @@ class _BottomButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: cb,
-      child: Container(
-        // margin: EdgeInsets.all(4),
-        height: 40,
-        width: 40,
-        decoration: BoxDecoration(
-            border:
-                selected ? Border.all(color: Theme.of(context).accentColor, width: 4) : null,
-            borderRadius: BorderRadius.circular(12),
-            color: Theme.of(context).backgroundColor),
-        child: icon,
+      child: Neumorphic(
+        style: selected
+            ? NeumorphicStyle(
+              shape: NeumorphicShape.concave,
+              surfaceIntensity: 0.5,
+              intensity: 0.25,
+              depth: 8,
+                border: NeumorphicBorder(
+                color: NeumorphicTheme.currentTheme(context).accentColor,
+                width: 4,
+              ))
+            : null,
+        child: Container(
+          // margin: EdgeInsets.all(4),
+          height: 40,
+          width: 40,
+          child: icon,
+        ),
       ),
     );
   }
