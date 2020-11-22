569345c3c0fcafe5ab2f2a1c8d2b0465d64b6587
Trevor Plant <tplant@dius.com.au>
2020-11-22 15:23:46 +1100

    Add Neumorphic package and refactor app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:neu_clock/core/theme/app_theme.dart';
 import 'package:neu_clock/presentation/screens/clock_screen.dart';
 
@@ -9,10 +10,18 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return NeumorphicApp(
       debugShowCheckedModeBanner: false,
       title: 'Neu Clock',
-      theme: appThemeData(),
+    
+      theme: NeumorphicThemeData(
+        baseColor: Color(0xFFEFEAEA),
+        accentColor: Color(0xFFB9ABAB),
+        defaultTextColor: Color(0xFF806A6A),
+        lightSource: LightSource.topLeft,
+        depth: 10,
+        intensity: 0.7,
+      ),
       home: ClockScreen(),
     );
   }
--- lib/presentation/screens/clock_screen.dart
+++ lib/presentation/screens/clock_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:neu_clock/models/alarm.dart';
 import 'package:neu_clock/presentation/widgets/alarm_list.dart';
 import 'package:neu_clock/presentation/widgets/bottom_nav_bar.dart';
@@ -8,40 +9,49 @@ class ClockScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      appBar: NeumorphicAppBar(
         title: Text(
           'Alarm',
           style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 24,
-              color: Theme.of(context).primaryColor),
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+            color: NeumorphicTheme.currentTheme(context).defaultTextColor,
+          ),
         ),
-        backgroundColor: Theme.of(context).backgroundColor,
-        elevation: 0,
+        // backgroundColor: Theme.of(context).backgroundColor,
+        // elevation: 0,
         actions: [
-          IconButton(
-            icon: Icon(Icons.settings, color: Theme.of(context).primaryColor),
+          NeumorphicButton(
+            margin: EdgeInsets.only(top: 4),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: NeumorphicIcon(
+                Icons.settings,
+                size: 32,
+                style: NeumorphicStyle(
+                    color:
+                        NeumorphicTheme.currentTheme(context).defaultTextColor),
+              ),
+            ),
             onPressed: () => {},
-            iconSize: 32,
           )
         ],
       ),
       body: Container(
-        color: Theme.of(context).backgroundColor,
+        // color: Theme.of(context).backgroundColor,
         child: SafeArea(
           child: Column(
             children: [
               Container(
                 height: 300,
-               
                 child: Clock(clockFaceSize: 250),
               ),
               Container(
                 height: 200,
                 child: Center(
-                  child: AlarmList(alarms: alarmList()..sort((a,b) => a.comparedTo(b))),
+                  child: AlarmList(
+                      alarms: alarmList()..sort((a, b) => a.comparedTo(b))),
                 ),
-                
               ),
               Expanded(child: Container()),
               Container(
@@ -56,9 +66,8 @@ class ClockScreen extends StatelessWidget {
   }
 }
 
- List<Alarm> alarmList() => <Alarm>[
-   Alarm(time: TimeOfDay(hour: 6, minute: 25), enabled: true),
-   Alarm(time: TimeOfDay(hour: 19, minute: 15), enabled: false),
-   Alarm(time: TimeOfDay(hour: 7, minute: 45), enabled: true),
-   
- ];
+List<Alarm> alarmList() => <Alarm>[
+      Alarm(time: TimeOfDay(hour: 6, minute: 25), enabled: true),
+      Alarm(time: TimeOfDay(hour: 19, minute: 15), enabled: false),
+      Alarm(time: TimeOfDay(hour: 7, minute: 45), enabled: true),
+    ];
